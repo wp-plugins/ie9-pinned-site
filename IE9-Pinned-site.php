d9d76dfd45e21c0bbb64761a663eb6cf7898f6a5
fatihboy <fatihboy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-26 19:16:13 +0000

    version update
    git-svn-id: https://plugins.svn.wordpress.org/ie9-pinned-site/trunk@293805 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- IE9-Pinned-site.php
+++ IE9-Pinned-site.php
@@ -42,9 +42,9 @@ function ie9_pinned_site_init() {
 }
 
 function ie9_pinned_site_head() {
-	echo "\r\n" . '<!-- IE9 Pinned Site 1.0.0 by Fatih Boy -->'."\r\n";
+	echo "\r\n" . '<!-- IE9 Pinned Site 1.0.1 by Fatih Boy -->'."\r\n";
 	echo '<!-- info : http://www.enterprisecoding.com/blog/projects/ie9-pinned-site -->'."\r\n";
-	echo '<meta name="IE9 Pinned Site" content="1.0.0" />'."\r\n";
+	echo '<meta name="IE9 Pinned Site" content="1.0.1" />'."\r\n";
 	
 	if ( strpos( $_SERVER['HTTP_USER_AGENT'], 'MSIE 9' ) !== FALSE ) {
 		echo '<meta name="application-name" content="' . get_bloginfo( 'name' ) . '" />'."\r\n";
